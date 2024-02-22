00e2ac4f3b6917db644656ce0abe632323afe41a
Vitaly8414 <140904739+Vitaly8414@users.noreply.github.com>
2024-02-22 20:38:25 +0600

    Update settings1.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 from django.conf.global_settings import MEDIA_ROOT, MEDIA_URL
 
+import os
+
 from dotenv import load_dotenv  
 
 load_dotenv()
@@ -26,9 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
-
-# 'django-insecure-ki)fobhh$=f(#0*5&8*(sdymua0f#p+!+%l-sgyxwy6kn%+a17'
+SECRET_KEY = os.environ.get("django-insecure-ki)fobhh$=f(#0*5&8*(sdymua0f#p+!+%l-sgyxwy6kn%+a17")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG"))
